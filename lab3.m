6727c0812aec1811d09a171ef84aae71ed2cba2e
Lai Weng Han <jlwhoo7@gmail.com>
2018-08-31 09:44:21 +0800

    update with einstein birthday

--- lab3.m
+++ lab3.m
@@ -1,23 +1,22 @@
 % set time to be 4000 HZ
 cover_time = ( 1 / 4000 );
-% Generate 50 samples
-tt = [-cover_time:cover_time/24.5:cover_time]; 
+% Generate 100 samples
+tt = -cover_time:cover_time/99.5:cover_time; 
 
-A1 = 22; % your age
-M = 6; % birthday month
-D = 22; % birthday day
+% einstein birthday: 14/3/1879
+A1 = 139; % your age
+M = 3; % birthday month
+D = 14; % birthday day
 T = tt; % period
 
 % x1
-tm1 = (37.2/M)*T
+tm1 = (37.2/M)*T;
 x1 = A1*cos(2*pi*4000*(tt-tm1));
 
 % x2
 A2 = 1.2*A1;
-tm2 = - (41.3/D)*T 
+tm2 = - (41.3/D)*T;
 x2 = A2*cos(2*pi*4000*(tt-tm2));
-
-suptitle('NAME: Lai Weng Han');
 
 subplot(3,1,1);
 plot(tt, x1);
@@ -31,4 +30,18 @@ x3 = x1 + x2;
 
 subplot(3,1,3);
 plot(tt, x3);
-title('x3');
+title('x3 = x1 + x2');
+
+% make title
+% https://stackoverflow.com/questions/33593964/title-over-group-of-subplots
+a = axes;
+t = title('NAME: Einstein');
+a.Visible = 'off';
+t.Visible = 'on';
+
+% x1
+% amplitude 139
+% 
+
+% x2
+% amplitude 166.8
