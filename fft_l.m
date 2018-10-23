7e4fba276f5f725908c9cf59c8f4760ad3762f74
Lai Weng Han <jlwhoo7@gmail.com>
2018-10-23 18:24:46 +0800

    fix

--- fft_l.m
+++ fft_l.m
@@ -6,10 +6,11 @@ L = 500;
 f = ((0:L-1)/L)*Fs;
 % 3
 k=1;
-w = 2*pi*Fs;
+w = 2*pi;
 xt = (4/pi)*(1/k)*sin(w*k*t);
 % 4
 X = fft(xt,L);
 % 5
 Xf = 2*abs(X)/L;
 plot(f(1:(L/2)),Xf(1:L/2));
+plot(xt,t);
